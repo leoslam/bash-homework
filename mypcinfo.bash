8dfded27b1ff97c247aa986be85f12774b14b52a
Leonardo Pavez <leonardopavez@MacBook-Pro-de-Leonardo.local>
2020-06-05 00:37:26 -0400

    agrega variables a la tarea

--- mypcinfo.bash
+++ mypcinfo.bash
@@ -1,4 +1,8 @@
-sysctl -n hw.logicalcpu_max
-system_profiler SPHardwareDataType | grep 'Processor Name'
-system_profiler SPHardwareDataType | grep Memory
+Variable1=$(sysctl -n hw.logicalcpu_max)
+Variable2=$(system_profiler SPHardwareDataType | grep 'Processor Name')
+Variable3=$(system_profiler SPHardwareDataType | grep Memory)
 
+
+echo "Número de procesadores: $Variable1";
+echo "Nombre del Procesador: $Variable2";
+echo "Memoria RAM: $Variable3";
