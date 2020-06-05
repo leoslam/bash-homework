679b8a05fd1631e8da0950556a993c79234d414b
Leonardo Pavez <leonardopavez@MacBook-Pro-de-Leonardo.local>
2020-06-05 00:51:44 -0400

    eliminar caracteres indeseables

--- mypcinfo.bash
+++ mypcinfo.bash
@@ -1,8 +1,8 @@
 Variable1=$(sysctl -n hw.logicalcpu_max)
-Variable2=$(system_profiler SPHardwareDataType | grep 'Processor Name')
-Variable3=$(system_profiler SPHardwareDataType | grep Memory)
+Variable2=$(system_profiler SPHardwareDataType | grep 'Processor Name' | cut '-d' ":" -f2 | sed s'/ //')
+Variable3=$(system_profiler SPHardwareDataType | grep Memory | cut '-d' ":" -f2 | sed s'/ //')
 
 
 echo "Número de procesadores: $Variable1";
 echo "Nombre del Procesador: $Variable2";
-echo "Memoria RAM: $Variable3";
+echo "Memoria RAM (GB): $Variable3";
