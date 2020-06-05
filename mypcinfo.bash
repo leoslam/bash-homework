9243ea7a15e3c8a662e5539f2b398586ed7b7333
Leonardo Pavez <leonardopavez@MacBook-Pro-de-Leonardo.local>
2020-06-05 12:16:23 -0400

    info binario y para ios

--- mypcinfo.bash
+++ mypcinfo.bash
@@ -1,3 +1,6 @@
+#Bash/bin
+#Comandos para IOS
+
 Variable1=$(sysctl -n hw.logicalcpu_max)
 Variable2=$(system_profiler SPHardwareDataType | grep 'Processor Name' | cut '-d' ":" -f2 | sed s'/ //')
 Variable3=$(system_profiler SPHardwareDataType | grep Memory | cut '-d' ":" -f2 | sed s'/ //')
